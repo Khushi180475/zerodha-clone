191cc6a026e730bba682db362fcb13fdceb0f35f
Khushi180475 <khushiaggarwal726@gmail.com>
2026-06-08 11:06:54 +0530

    Update CORS for production

--- backend/index.js
+++ backend/index.js
@@ -14,8 +14,7 @@ app.use(cors({
   origin: [
     "http://localhost:3001",
     "http://localhost:3000",
-    "https://zerodha-frontend.vercel.app",
-    "https://zerodha-dashboard.vercel.app"
+    "https://zerodha-clone-kappa-liard.vercel.app"
   ]
 }));
 
